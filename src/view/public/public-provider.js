e3f70dab676fa62e59d0d52367331405cdd8fc76
junjie-lean <40194410+junjie-lean@users.noreply.github.com>
2020-07-28 10:02:02 +0800

    bug fixed

--- src/view/public/public-provider.js
+++ src/view/public/public-provider.js
@@ -2,7 +2,7 @@
  * @Author: junjie.lean
  * @Date: 2020-03-17 09:52:08
  * @Last Modified by: junjie.lean
- * @Last Modified time: 2020-07-20 09:57:03
+ * @Last Modified time: 2020-07-28 10:01:59
  */
 
 import React, { createContext } from "react";
@@ -28,7 +28,7 @@ export default function App() {
   return (
     <PerformanceMonitor>
       <Antd.ConfigProvider locale={zhCN}>
-        <Context.Provider store={store}>
+        <Context.Provider value={store}>
           <RouterRelation />
         </Context.Provider>
       </Antd.ConfigProvider>
